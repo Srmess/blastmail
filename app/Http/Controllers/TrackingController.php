a99b020e0db2ffa7813b098e062c0632f0367a7a
Srmess <boynerd@outlook.com>
2025-08-27 10:09:14 -0300

    [FEAT]: Adding track validation

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -9,14 +9,18 @@ class TrackingController extends Controller
 {
     public function opening(CampaignMail $mail)
     {
-        $mail->openings++;
-        $mail->save();
+        if ($mail->campaign->track_open) {
+            $mail->openings++;
+            $mail->save();
+        }
     }
 
     public function click(CampaignMail $mail)
     {
-        $mail->clicks++;
-        $mail->save();
+        if ($mail->campaign->track_click) {
+            $mail->clicks++;
+            $mail->save();
+        }
 
         return redirect()->away(request()->get('foward'));
     }
